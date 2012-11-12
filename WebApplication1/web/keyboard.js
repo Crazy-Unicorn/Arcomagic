d27e3abde4d8bec879c5afb42c7c7f1a6da096eb
villionk <villionk@villionk.nedra.ru>
2012-11-12 16:34:24 +0300

    keyboard

--- WebApplication1/web/keyboard.js
+++ WebApplication1/web/keyboard.js
@@ -2,7 +2,7 @@
 backgroundColor = "black";
 keyColor = "white";
 
-name = "Player";
+name = "";
 
 function drawKeyboard () {
 
@@ -65,36 +65,238 @@ function keyboardShow() {
         };
 
         field = new Object();
+             
+	var str='';
+		
+	var delta=50;
+        var shift=70;
+	var size_buttom=35;
+	var otctup_h=260;
+	var otctup_h2=305;
+	var otctup_h3=350;
+	var otctup_h4=395;
+	var str2_otctup=19;
         
         field.draw = function() {
             bctx.beginPath();
             bctx.strokeStyle = keyColor; 
-            bctx.strokeRect(10, 10, 500, 100);
+            bctx.strokeRect(delta*4, otctup_h/3, size_buttom*10+delta, 75);
             bctx.closePath();
             
             bctx.beginPath();
-            bctx.font = "20pt Arial";
+            bctx.font = "40pt Lucida Console";
             bctx.textAlign = "left";
             bctx.fillStyle = keyColor;
-            bctx.fillText(name, 20, 60);
+            bctx.fillText(name, delta*4+size_buttom, otctup_h/3+50);
             bctx.closePath();
             
+            if(name.length<10){
             bctx.beginPath();
             bctx.fillStyle = keyColor;
-            bctx.rect(109, 61, 30, 10);
+            bctx.rect(delta*4+size_buttom+name.length*32, otctup_h/3+50, 30, 10);
             bctx.fill();
-            bctx.closePath();
+            bctx.closePath();}
         };
-
-        buttons.push(newButton(10,410,40,40,'q').addEvent('mousedown', function() {
-            alert('q!')
+	
+	//name=name+'qwe';
+	 
+        buttons.push(newButton(1*delta+shift,otctup_h,size_buttom,size_buttom,'q').addEvent('mousedown', function() {
+            //alert('q!');
+             if(name.length<10){
+                 name=name+'q';
+                 drawKeyboard();                  
+             }                         
         }));
-        buttons.push(newButton(70,410,40,40,'w').addEvent('mousedown', function() {
-            alert('w!')
+        buttons.push(newButton(2*delta+shift,otctup_h,size_buttom,size_buttom,'w').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'w';
+                 drawKeyboard();                  
+             } 
         }));
-        buttons.push(newButton(130,410,80,40,'Enter').addEvent('mousedown', function() {
-            alert('Enter')
+	buttons.push(newButton(3*delta+shift,otctup_h,size_buttom,size_buttom,'e').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'e';
+                 drawKeyboard();                  
+             } 
         }));
+        buttons.push(newButton(4*delta+shift,otctup_h,size_buttom,size_buttom,'r').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'r';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(5*delta+shift,otctup_h,size_buttom,size_buttom,'t').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'t';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(6*delta+shift,otctup_h,size_buttom,size_buttom,'y').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'y';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(7*delta+shift,otctup_h,size_buttom,size_buttom,'u').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'u';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(8*delta+shift,otctup_h,size_buttom,size_buttom,'i').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'i';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(9*delta+shift,otctup_h,size_buttom,size_buttom,'o').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'o';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(10*delta+shift,otctup_h,size_buttom,size_buttom,'p').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'p';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(11*delta+shift,otctup_h,size_buttom*1.5,size_buttom,'Del').addEvent('mousedown', function() {
+            if(name.length>0){
+                name=name.substring(0, name.length-1);
+                drawKeyboard();
+            }
+        }));
+	
+	buttons.push(newButton(1*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'a').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'a';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(2*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'s').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'s';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(3*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'d').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'d';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(4*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'f').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'f';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(5*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'g').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'g';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(6*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'h').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'h';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(7*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'j').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'j';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(8*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'k').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'k';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(9*delta+str2_otctup+shift,otctup_h2,size_buttom,size_buttom,'l').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'l';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(10*delta+str2_otctup+shift,otctup_h2,size_buttom*2,size_buttom,'Enter').addEvent('mousedown', function() {
+            if(name.length<10){
+                 //name=name+'q';
+                 drawKeyboard();                  
+             } 
+        }));
+	
+	buttons.push(newButton(1*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'z').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'z';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(2*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'x').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'x';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(3*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'c').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'c';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(4*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'v').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'v';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(5*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'b').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'b';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(6*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'n').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'n';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(7*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'m').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'m';
+                 drawKeyboard();                  
+             } 
+        }));
+        buttons.push(newButton(8*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'_').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'_';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(9*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'.').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'.';
+                 drawKeyboard();                  
+             } 
+        }));
+	buttons.push(newButton(10*delta+str2_otctup*2+shift,otctup_h3,size_buttom,size_buttom,'-').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+'-';
+                 drawKeyboard();                  
+             } 
+        }));
+	
+	buttons.push(newButton((10*delta-delta)/2+shift,otctup_h4,size_buttom*6,size_buttom,' ').addEvent('mousedown', function() {
+            if(name.length<10){
+                 name=name+' ';
+                 drawKeyboard();                  
+             } 
+        }));
+	
         drawKeyboard();
     }
 }
@@ -122,7 +324,7 @@ function Button(x, y, width, height, label) {
             var x_to = this.el.x+this.el.width;
             var y_from = this.el.y;
             var y_to = this.el.y+this.el.height;
-            alert(cX + ' ' + cY + ' ' + x_from + ' ' + x_to + ' ' + y_from + ' ' + y_to)
+            //alert(cX + ' ' + cY + ' ' + x_from + ' ' + x_to + ' ' + y_from + ' ' + y_to)
             if (cX>=x_from && cX<=x_to && cY>=y_from && cY<=y_to) {
                 //alert(2)
                 this.el.events[evt.type].processEvent();
@@ -139,10 +341,11 @@ function Button(x, y, width, height, label) {
             bctx.closePath();
             
             bctx.beginPath();
-            bctx.font = "20pt Arial";
+	    var fontHeight = 20;
+            bctx.font = fontHeight+"px Arial";
             bctx.textAlign = "center";
             bctx.fillStyle = keyColor;
-            bctx.fillText(label, x+width/2, y+height/2);
+            bctx.fillText(label, x+width/2, y+height/2+fontHeight/4);
             bctx.closePath();
     }
 }
